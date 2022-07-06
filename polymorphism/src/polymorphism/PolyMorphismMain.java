3403618a2a2e8dd8cf5ef1cb786f3a81734d56f7
tuan <jhmh0226@gmail.com>
2022-07-06 16:56:14 +0900

    20220706

--- polymorphism/src/polymorphism/PolyMorphismMain.java
+++ polymorphism/src/polymorphism/PolyMorphismMain.java
@@ -1,5 +1,7 @@
 package polymorphism;
 
+import java.util.ArrayList;
+
 import polymorphism.vo.*;
 
 public class PolyMorphismMain {
@@ -11,14 +13,38 @@ public class PolyMorphismMain {
 		
 		Parent p = new TheFirst();
 		p.viewPersonality();
+		System.out.println();
 		
 		System.out.println("---------------------");
 		p = new TheSecond();
 		p.viewPersonality();
+		System.out.println();
 		
 		System.out.println("---------------------");
 		p = new TheThrid();
 		p.viewPersonality();
+		System.out.println();
+		
+		System.out.println("-------클래스 배열-------");
+		Parent[] children = new Parent[3];
+		children[0] = new TheFirst();
+		children[1] = new TheSecond();
+		children[2] = new TheThrid();
+		
+		for(Parent c : children) {
+			c.viewPersonality();
+		}
+		System.out.println();
+		
+		System.out.println("-------ArrayList-------");
+		ArrayList<Parent> children2 = new ArrayList<Parent>();
+		children2.add(new TheFirst());
+		children2.add(new TheSecond());
+		children2.add(new TheThrid());
+		
+		for(Parent c : children2) {
+			c.viewPersonality();
+		}
 	}
 
 }
