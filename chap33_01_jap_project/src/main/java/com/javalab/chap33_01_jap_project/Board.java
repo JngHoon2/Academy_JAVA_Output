3238c5787c6554918004261acb36c359b566f4ec
tuan <jhmh0226@gmail.com>
2022-11-24 17:49:47 +0900

    20221124

--- chap33_01_jap_project/src/main/java/com/javalab/chap33_01_jap_project/Board.java
+++ chap33_01_jap_project/src/main/java/com/javalab/chap33_01_jap_project/Board.java
@@ -6,87 +6,77 @@ import java.util.Date;
 import javax.persistence.*;
 
 /**
- * Entity implementation class for Entity: Board
+ * Entity 클래스
  * - 엔티티 클래스를 영속 영역에서 관리하게 된다.
  */
-@Entity
+@Entity	// 나는 JPA에서 관리하는 엔티티 클래스임.
 @Table(name="board")
 public class Board implements Serializable {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	// BoardInsertClientIdentity로 테스트
 	private int seq;
 	
-	// 매핑 정보가 없는 필드들은 자동으로 테이블의 컬럼과 매
+	// 매핑 정보가 없는 필드들은 자동으로 테이블의 컬럼과 매핑
 	private String title;
 	private String writer;
-	private String content;
+	private String content;	
 	private int cnt;
 	private int category_no;
 	
+	// 날짜 타입의 변수에 선언하여 날짜 타입 매핑
 	@Temporal(TemporalType.DATE)
 	private Date regDate = new Date();
-    
+	
 	public int getCategory_no() {
 		return category_no;
 	}
-	
 	public void setCategory_no(int category_no) {
 		this.category_no = category_no;
 	}
-
+	
 	public int getSeq() {
 		return seq;
 	}
-
 	public void setSeq(int seq) {
 		this.seq = seq;
 	}
-
 	public String getTitle() {
 		return title;
 	}
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
 	public String getWriter() {
 		return writer;
 	}
-
 	public void setWriter(String writer) {
 		this.writer = writer;
 	}
-
 	public String getContent() {
 		return content;
 	}
-
 	public void setContent(String content) {
 		this.content = content;
 	}
-
-	public int getCnt() {
-		return cnt;
-	}
-
-	public void setCnt(int cnt) {
-		this.cnt = cnt;
-	}
-
 	public Date getRegDate() {
 		return regDate;
 	}
-
 	public void setRegDate(Date regDate) {
 		this.regDate = regDate;
 	}
-
+	public int getCnt() {
+		return cnt;
+	}
+	public void setCnt(int cnt) {
+		this.cnt = cnt;
+	}
 	@Override
 	public String toString() {
-		return "Board [seq=" + seq + ", title=" + title + ", writer=" + writer + ", content=" + content + ", cnt=" + cnt
-				+ ", category_no=" + category_no + ", regDate=" + regDate + "]";
-	}
-
+		return "Board [seq=" + seq + ", title=" + title + ", writer=" + writer + ", content=" + content + ", regDate="
+				+ regDate + ", cnt=" + cnt + "]";
+	}	
+	
+	
+	
 }
--- chap33_01_jap_project/src/test/java/com/javalab/chap33_01_jap_project/BoardInsertClient.java
+++ chap33_01_jap_project/src/test/java/com/javalab/chap33_01_jap_project/BoardInsertClient.java
@@ -9,10 +9,10 @@ import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 
 public class BoardInsertClient {
-
+	
 	public static void main(String[] args) {
 		
-		// EntitiyManger 생성
+		// EntityManager 생성("chap30_jpa_project" : persistence.xml에 설정한 persistence-unit name)
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("chap33_01_jap_project");
 		EntityManager em = emf.createEntityManager();
 		
@@ -26,8 +26,8 @@ public class BoardInsertClient {
 			Board board = new Board();
 			board.setTitle("JPA 제목-1");
 			board.setWriter("관리자-1");
-			board.setContent("JPA 관련 게시물의 내용힙니다.-1");
-			
+			board.setContent("JPA 관련 게시물의 내용입니다.-1");
+
 			// [3] 글등록(영속화-영속컨텍스트 즉, 영속영역에 저장)
 			// 영속컨텍스트는 트랜잭션 범위 안에서 유지가 된다.
 			em.persist(board);
@@ -35,42 +35,56 @@ public class BoardInsertClient {
 			
 			// [4] Transaction commit
 			System.out.println("commit 실행 직전-2");
-			tx.commit(); // commit 실행 시 DB에 저장.
+			tx.commit();	// 비로소 데이터베이스에 저장SQL 실행된다.			
 			System.out.println("commit 실행 직후-3");
 			
-			// [5] 글 목록 조회
-			// [5.1] jpql 쿼리 생성, JPQL은 SQL 과 비슷한 문법을 가진 객체지향 쿼리.
-			// Java Persistence Query Language
-			// - from Board b : Board는 자바의 엔티티 클래스이며 b는 엔티티 클래스의 alias(별칭)임
-			String jpql = "select b from Board b order by b.seq desc"; //jpql
+			/*
+			 * [5] 글 목록 조회
+			 *  [5.1] JPQL 쿼리 생성, JPQL은 SQL과 비슷한 문법을 가진 객체 지향 쿼리임.
+			 *  - Java Persistence Query Language(일종의 객체지향 쿼리문)
+			 *  - from Board b : Board는 자바의 엔티티 클래스이며 b는 엔티티 클래스의 alias임(별칭) 
+			 */
+			String jpql = "select b from Board b order by b.seq desc"; // jpql
 			
-			// [5.2] 쿼리 객체 생성
-			// TypeQuery : 작성한 JPQL을 실행시키기 위해 만드는 쿼리 객체 
-	        // - 반환할 타입을 명확하게 지정할 수 있을 경우 사용
-	        // - 조회대상이 정확히 Board 엔티티이므로 TypedQuery 사용 가능
-	        // - SELECT b.title FROM Board b 도 TypedQuery사용 가능(조회 컬럼이 딱1개)
-	        // - em.createQuery(jpql, Board.class) : 두 번째 인자인 Board타입 반환
-	        // Query : 쿼리 결과 즉, 조회 컬럼이 String, Integer로 명확하지 않을때 사용
-	        // - Query query = SELECT b.title, b.cnt FROM Board b;
-	        // - 조회 컬럼이 1개일 경우 Object를 반환, 1개 이상일 경우 Object[]를 반환함
-			TypedQuery<Board> board2 = em.createQuery(jpql, Board.class);
+			/*
+			 * [5.2] 쿼리 객체 생성
+			 * TypeQuery : 작성한 JPQL을 실행시키기 위해 만드는 쿼리 객체 
+			 *  - 반환할 타입을 명확하게 지정할 수 있을 경우 사용
+			 *  - 조회대상이 정확히 Board 엔티티이므로 TypedQuery 사용 가능
+			 *  - SELECT b.title FROM Board b 도 TypedQuery사용 가능(조회 컬럼이 딱1개)
+			 *  - em.createQuery(jpql, Board.class) : 두 번째 인자인 Board타입 반환
+			 * Query : 쿼리 결과 즉, 조회 컬럼이 String, Integer로 명확하지 않을때 사용
+			 *  - Query query = SELECT b.title, b.cnt FROM Board b;
+			 *  - 조회 컬럼이 1개일 경우 Object를 반환, 1개 이상일 경우 Object[]를 반환함			 *  
+			 */
+			TypedQuery<Board> board2 = em.createQuery(jpql, Board.class); // 
+
+			/*
+			 * [5.3] 쿼리 객체 실행
+			 * board2.getResultList()
+			 *  - 결과를 컬렉션으로 반환한다. 결과가 없으면 빈 컬렉션이 반환된다. 
+			 *  - 1건이면 1건만 들어간 컬렉션이 반환된다.
+			 *    
+			 * query.getSingleResult()
+			 *  - 결과가 정확히 1건 일때 사용
+			 *  - 결과가 없으면 NoResultException, 
+			 *    결과가 1건 이상이면 NonUniqueResultException 발생
+			 */
+			List<Board> boardList = board2.getResultList(); // 쿼리 객체 실행
+			//List<Board> boardList = em.createQuery(jpql, Board.class).getResultList(); // 한줄로 작성시
 			
-			// [5.3] 쿼리 객체 실행
-			// board2.getResultList()
-			// - 결과를 컬렉션으로 반환한다. 결과가 없으면 빈 컬렉션을 반환한다.
-			// - 1건이면 1건만 들어간 컬렉션이 반환된다.
-	        //  query.getSingleResult()
-	        // - 결과가 정확히 1건 일때 사용
-	        // - 결과가 없으면 NoResultException, 
-	        // - 결과가 1건 이상이면 NonUniqueResultException 발생
-			List<Board> boardList = board2.getResultList();
-			// List<Board> boardList = em.createQuery(jpql, Board.class).getResultList(); 이런 식으로도 사용 가능하다.
-			
+			for (Board brd : boardList) {
+				System.out.println("---> " + brd.toString());
+			}
 			
 		} catch (Exception e) {
-			// TODO: handle exception
+			e.printStackTrace();
+			// Transaction rollback
+			tx.rollback();
+		} finally {
+			em.close();
 		}
-
+		
+		emf.close();
 	}
-
 }
